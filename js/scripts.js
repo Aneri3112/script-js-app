440bc73875340020577cfc470a9c39ce83b43ab8
Aneri3112 <aneri.pate@hotmail.com>
2022-03-10 19:49:10 +0100

    Fix spacing issue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonlist = [
-    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poision']},
-    {name: 'caterpie', height: '0.3', types: 'bug'},
-    {name: 'jihhlypuff',height: '0.5',types: ['fairy', 'normal']}
+ { name: 'Bulbasaur', height: '0.7', types: ['grass', 'poision']},
+ { name: 'caterpie', height: '0.3', types: ['bug']},
+ { name: 'jihhlypuff', height:'0.5', types: ['fairy', 'normal']}
 ];
