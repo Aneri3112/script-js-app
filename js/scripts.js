a6a63f3bbd3e77f29f3a012d99dbbe80a3e5272d
Aneri3112 <aneri.pate@hotmail.com>
2022-03-10 16:03:54 +0100

    Update scripts.js by adding Array & object.
    Added arrays and object to variable name pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello World');
+let pokemonlist = [{
 
-let favouritefood = 'Käsespätzle';
-document.write(favouritefood);
+        name: 'Bulbasaur',
+        height: '0.7',
+        type: ['grass', 'poision']
+    },
+
+    {
+        name: 'caterpie',
+        height: '0.3',
+        type: 'bug'
+    },
+
+    {
+        name: 'jihhlypuff',
+        height: '0.5',
+        type: ['fairy', 'normal']
+    }
+];
