a83b8a51f845b58d4d1ca4e86a71935bbfb7352d
Aneri3112 <aneri.pate@hotmail.com>
2022-03-11 14:56:14 +0100

    Added for loop and condition.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonlist = [
  { name: 'caterpie', height: '0.3', types: ['bug']},
  { name: 'jihhlypuff', height:'0.5', types: ['fairy', 'normal']}
 ];
+
+//creating for-loop for pokemonlist    
+for(let i=0; i < pokemonlist.length; i++) {
+    if (pokemonlist[i].height > 0.5){
+        document.write( `${pokemonlist[i].name + " "} (height: ${ pokemonlist[i].height}) - Wow, that's big!` + " ");
+    }else {
+       document.write( `${pokemonlist[i].name + " " } (height: ${ pokemonlist[i].height}) ` + " "); 
+    }
+}
